eceb4403471f4286e7cb5755a2a29e54f8019f1f
Anna <132434495+Anna1342@users.noreply.github.com>
2026-03-16 23:07:51 +0100

    Add Case class with item handling and effects

--- Projet_minimal/Case.java
+++ Projet_minimal/Case.java
@@ -1,1 +1,33 @@
 
+package entites;
+
+public class Case {
+    private int x; 
+    private int y; 
+    private Item item; 
+
+    public Case(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void appliquerEffet(Joueur joueur) { 
+        if (this.item != null) {
+            joueur.appliquerItem(this.item);
+            // Les bonus ne sont pas renouvelables 
+            if (this.item.getValeur() >0) {
+                this.item = null;
+            }
+        }
+    }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+    
+    public void setItem(Item item) { 
+    	this.item = item; }
+
+	public Item getItem() {
+		return this.item;
+	}
+}
