21b0497bed0cc368a7d6ca19530a37e035dad0e2
Anna <132434495+Anna1342@users.noreply.github.com>
2026-03-16 23:57:39 +0100

    Add Predateur class with energy loss functionality

---
+++ Projet_interface/Predateur.java
@@ -0,0 +1,15 @@
+package entites;
+
+public class Predateur {
+	private int malusplus;
+
+	public int perteEnergie() {
+		return malusplus;
+	}
+
+	public Predateur(int malusplus) {
+		super();
+		this.malusplus = malusplus;
+	}
+
+	}
