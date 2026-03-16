5ca56349e30ec308ac1bbb787cdda8af7ed3fb1a
Anna <132434495+Anna1342@users.noreply.github.com>
2026-03-16 23:54:29 +0100

    Add Fruit class with energy gain functionality

---
+++ Projet_interface/Fruit.java
@@ -0,0 +1,17 @@
+package entites;
+
+public class Fruit {
+	private int bonus;
+
+	public int gainEnergie() {
+		return bonus;
+	}
+
+	
+	public Fruit(int fruit) {
+		super();
+		this.bonus = fruit;
+	}
+	
+
+}
