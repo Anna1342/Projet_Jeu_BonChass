d71109f035d40c265e69516e4e2f90fc2eb77a9d
Anna <132434495+Anna1342@users.noreply.github.com>
2026-03-16 23:54:05 +0100

    Add Feu class with malus attribute and method

---
+++ Projet_interface/Feu.java
@@ -0,0 +1,16 @@
+
+package entites;
+
+public class Feu {
+	private int malus;
+
+	public Feu(int malus) {
+		super();
+		this.malus = malus;
+	}
+
+	public int energieMoins() {
+		return malus;
+	}
+}
+
