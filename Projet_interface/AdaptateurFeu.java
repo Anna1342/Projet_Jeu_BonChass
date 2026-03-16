c109dd7a7793f9da0f0ea539de9efdcff8096cc2
Anna <132434495+Anna1342@users.noreply.github.com>
2026-03-16 23:48:29 +0100

    Add AdaptateurFeu class to handle Feu energy

--- Projet_interface/AdaptateurFeu.java
+++ Projet_interface/AdaptateurFeu.java
@@ -1,1 +1,15 @@
+package entites;
 
+public class AdaptateurFeu implements Item {
+	
+    private Feu feu; //On défini un objet feu de classe Feu
+    
+    public AdaptateurFeu(Feu feu) { 
+    	this.feu = feu; 
+    	}
+    
+    @Override
+    public int getEnergie() { 
+		return -feu.energieMoins(); //Renvoie la valeur de l'énergie qui doit être retranché au joueur
+	}
+}
